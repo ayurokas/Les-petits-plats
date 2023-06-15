4d25635f9b95e4161993b42754368363289ec405
perso <123567133+ayurokas@users.noreply.github.com>
2023-06-15 14:25:33 +0200

    Update index.js

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -3,37 +3,37 @@ let filteredRecipes = recipes;
 
 // Fonction pour obtenir tous les ingrédients
 function getAllIngredients() {
-    const ingredients = filteredRecipes.map(recipe => recipe.ingredients.map(obj => obj.ingredient))
-    const uniqueArray = [...new Set([...ingredients.flat()])];
+    const ingredients = filteredRecipes.map(recipe => recipe.ingredients.map(obj => obj.ingredient));
+    const uniqueArray = [...new Set(ingredients.flat())];
 
-    return uniqueArray
+    return uniqueArray;
 }
 
 // Fonction pour obtenir tous les appareils
 function getAllAppareils() {
-    const appareils = filteredRecipes.map(recipe => recipe.appliance)
-    const uniqueArray = [...new Set([...appareils])];
+    const appareils = filteredRecipes.map(recipe => recipe.appliance);
+    const uniqueArray = [...new Set(appareils)];
 
-    return uniqueArray
+    return uniqueArray;
 }
 
 // Fonction pour obtenir tous les ustensiles
 function getAllUstensiles() {
     const ustensiles = filteredRecipes.map(recipe => recipe.ustensils);
-    const uniqueArray = [...new Set([...ustensiles.flat()])];
+    const uniqueArray = [...new Set(ustensiles.flat())];
 
-    return uniqueArray
+    return uniqueArray;
 }
 
 // Fonction pour créer les éléments de filtre (ingrédients, appareils, ustensiles)
 function createFilterItems(id, items) {
-    const dropdown = document.getElementById(`${id}-dropdown`)
+    const dropdown = document.getElementById(`${id}-dropdown`);
     const filterItems = document.createElement("ul");
     filterItems.setAttribute("class", "filter-items");
     dropdown.innerHTML = '';
 
     dropdown.appendChild(filterItems);
-    items.map(item => {
+    items.forEach(item => {
         const li = document.createElement("li");
         li.onclick = function () {
             const results = searchRecipes(item);
@@ -50,8 +50,8 @@ function createFilterItems(id, items) {
         li.innerText = item;
         li.className = id;
         filterItems.appendChild(li);
-    })
-};
+    });
+}
 
 // Fonction pour rechercher des recettes en fonction d'un mot-clé
 function searchRecipes(keyword) {
@@ -90,11 +90,11 @@ function displayRecipes(recipes) {
     if (recipes.length === 0) {
         noRecipe.className = 'no-recipe visible';
     } else {
-        recipes.map(recipe => {
+        recipes.forEach(recipe => {
             const recipeModel = new recipeFactory(recipe);
             const recipeCard = recipeModel.createRecipeCard();
             recipesSection.appendChild(recipeCard);
-        })
+        });
     }
 }
 
@@ -102,27 +102,27 @@ function searchIngredients(keyword) {
     keyword = keyword.toLowerCase().trim();
     const ingredients = getAllIngredients();
 
-    const filteredIngredients = ingredients.map(i => i.toLowerCase()).filter(i => i.includes(keyword))
+    const filteredIngredients = ingredients.map(i => i.toLowerCase()).filter(i => i.includes(keyword));
 
-    return filteredIngredients
+    return filteredIngredients;
 }
 
 function searchAppareils(keyword) {
     keyword = keyword.toLowerCase().trim();
     const appareils = getAllAppareils();
 
-    const filteredAppareils = appareils.map(a => a.toLowerCase()).filter(a => a.includes(keyword))
+    const filteredAppareils = appareils.map(a => a.toLowerCase()).filter(a => a.includes(keyword));
 
-    return filteredAppareils
+    return filteredAppareils;
 }
 
 function searchUstensiles(keyword) {
     keyword = keyword.toLowerCase().trim();
     const ustensiles = getAllUstensiles();
 
-    const filteredUstensiles = ustensiles.map(u => u.toLowerCase()).filter(u => u.includes(keyword))
+    const filteredUstensiles = ustensiles.map(u => u.toLowerCase()).filter(u => u.includes(keyword));
 
-    return filteredUstensiles
+    return filteredUstensiles;
 }
 
 function displayTag(keyword, type) {
@@ -173,7 +173,7 @@ document.addEventListener('DOMContentLoaded', function () {
         } else if (keyword.length < 3) {
             createFilterItems("ingredient", getAllIngredients());
         }
-    })
+    });
 
     // Gestionnaire d'événement pour la saisie dans le champ d'appareil
     appareilInput.addEventListener('keyup', function (event) {
@@ -185,7 +185,7 @@ document.addEventListener('DOMContentLoaded', function () {
         } else if (keyword.length < 3) {
             createFilterItems("appareil", getAllAppareils());
         }
-    })
+    });
 
     // Gestionnaire d'événement pour la saisie dans le champ d'ustensile
     ustensileInput.addEventListener('keyup', function (event) {
@@ -197,27 +197,27 @@ document.addEventListener('DOMContentLoaded', function () {
         } else if (keyword.length < 3 || event.target.value === "") {
             createFilterItems("ustensile", getAllUstensiles());
         }
-    })
+    });
 
     // Gestionnaire d'événement pour le clic sur la flèche d'ingrédient
     ingredientArrow.addEventListener('click', () => {
         createFilterItems("ingredient", getAllIngredients());
-    })
+    });
 
     // Gestionnaire d'événement pour le clic sur la flèche d'appareil
     appareilArrow.addEventListener('click', () => {
         createFilterItems("appareil", getAllAppareils());
-    })
+    });
 
     // Gestionnaire d'événement pour le clic sur la flèche d'ustensile
     ustensileArrow.addEventListener('click', () => {
         createFilterItems("ustensile", getAllUstensiles());
-    })
+    });
 
     // Gestionnaire d'événement pour le clic sur la croix de filtre sélectionné
     document.addEventListener('click', function (event) {
         if (event.target.matches('img.x-icon')) {
-            var selectedFilter = event.target.closest('.selected-filter');
+            const selectedFilter = event.target.closest('.selected-filter');
             selectedFilter.parentNode.removeChild(selectedFilter);
         }
     });
@@ -241,6 +241,6 @@ function init() {
     createFilterItems("appareil", getAllAppareils());
     createFilterItems("ustensile", getAllUstensiles());
     displayRecipes(recipes);
-};
+}
 
 init();
